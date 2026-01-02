0e555d9c9114fe73488405756e4d91abec414250
Gerson Gomes <gersoncastrogomes95@gmail.com>
2026-01-02 16:48:12 -0300

    refactor main file

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,6 @@ import { main } from './pages/main.js';
 const app = document.getElementById('app');
 import { footer } from './pages/footer.js';
 
-
-
-
-
-
 app?.appendChild(header);
 app?.appendChild(main);
 app?.appendChild(footer);
--- src/pages/main.ts
+++ src/pages/main.ts
@@ -1,5 +1,8 @@
 const main = document.createElement('main');
 const mainTitle = document.createElement('h1');
+const backVideoContainer = document.createElement('div');
+
+backVideoContainer.classList.add('back-video-container');
 
 mainTitle.textContent = 'Stratus e o algoritimo Multi-Raft'
 
